a1cee87d0cbf825c17e2d13c4edd37a609b24f2a
Priscila Ribeiro <priscila.henriques.ribeiro@gmail.com>
2020-05-24 19:21:30 -0300

    atualizacao da tela de login

--- EstrategiaConcursos/spec/login_spec.rb
+++ EstrategiaConcursos/spec/login_spec.rb
@@ -1,21 +1,5 @@
 describe 'Login' do
 
-    it 'Exibir uma mensagem de erro para o campo não obrigatório' do 
-        #Acessar a home da estratégia: https://www.estrategiaconcursos.com.br/.
-        visit 'https://www.estrategiaconcursos.com.br/'
-        page.find(:xpath,'//*[@id="b_home"]/header/div/div/div/div[1]/a').click
-        #fill_in 'email' , with: 'bielorrusiaribeiro@gmail.com'
-        click_button 'Entrar'
-        expect(find('#email-error').visible?).to be true
-        #expect(page).to have_content('Este campo é obrigatório.') 
-    
-        #email-error
-        #fill_in 'senha' , with: 'Ph@1@2@3'
-
-        
-    end 
-
-
     it 'Realizar o login com sucesso' do 
         #Acessar a home da estratégia: https://www.estrategiaconcursos.com.br/.
         visit 'https://www.estrategiaconcursos.com.br/'
@@ -27,6 +11,27 @@ describe 'Login' do
         #expect(page).to include('Você possui campos não preenchidos no seu cadastro. Vamos atualizá-los?')    
     
     end 
+
+   
+    it 'Exibir uma mensagem de erro para o campo não obrigatório' do 
+        #Acessar a home da estratégia: https://www.estrategiaconcursos.com.br/.
+        visit 'https://www.estrategiaconcursos.com.br/'
+        page.find(:xpath,'//*[@id="b_home"]/header/div/div/div/div[1]/a').click
+        #fill_in 'email' , with: 'bielorrusiaribeiro@gmail.com'
+        click_button 'Entrar'
+        expect(find('#email-error').visible?).to be true
+        #expect(page).to have_content('Este campo é obrigatório.') 
+    
+        #email-error
+        #fill_in 'senha' , with: 'Ph@1@2@3'       
+    end
+    
+
+
+
+
+
+
 
     
 
